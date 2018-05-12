de67b553a4c8508c61fb9e7a1d47f31c94624c1d
Ricardo M. Lima <ricardolima89@gmail.com>
2018-05-12 03:23:16 -0300

    Importing tasks dynamically and showing available on error

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import glob
+import importlib
+from sys import argv
 
 
 class Application():
@@ -37,13 +39,20 @@ class TaskManager():
             pieces = task[:-3].split('/')
             name = pieces[-1]
 
-            return (name, )
+            return name
 
         tasks = [map_task(task)
                  for task in glob.glob("tasks/*.py")]
 
         return tasks
 
-
     def run(self):
-        pass
+        parameters = argv
+        script = parameters[1]
+        try:
+            task = importlib.import_module('.' + script, 'tasks')
+            task_action = getattr(task, 'run_task')
+            task_action()
+        except ImportError:
+            print("There is no such task.")
+            print("Available tasks: " + ", ".join(self.tasks))
